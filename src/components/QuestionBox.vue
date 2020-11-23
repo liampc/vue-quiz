06857d878018454632794b90271cf79735159d6f
Lia <58094927+liampc@users.noreply.github.com>
2020-11-23 13:58:36 +0800

    edit choices group

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,9 +7,14 @@
 
             <hr class="my-4">
 
-            <p v-for="(answer, index) in answers" :key="index">
+            <!-- <p v-for="(answer, index) in answers" :key="index">
                 {{answer}}
-            </p>
+            </p> -->
+            <b-list-group>
+                <b-list-group-item v-for="(answer, index) in answers" :key="index">
+                    {{answer}}
+                </b-list-group-item>
+            </b-list-group>
 
             <b-button variant="primary" href="#">Submit</b-button>
             <b-button variant="success" href="#" @click="next">Next Question</b-button>
@@ -32,3 +37,9 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    .list-group {
+        margin-bottom: 15px;
+    }
+</style>
