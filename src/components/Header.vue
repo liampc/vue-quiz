517f141a12cba9690e18f3b8c90a3244e66fe6d3
Lia <58094927+liampc@users.noreply.github.com>
2020-11-23 09:31:50 +0800

    add navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1> Header </h1>
+    <div>
+        <b-navbar type="light" variant="info">
+            <b-navbar-nav>
+            <b-nav-item >Hollywoo stars and celebrities what do they know </b-nav-item>
+            <b-nav-item> Counter 4/10 </b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
 </template>
